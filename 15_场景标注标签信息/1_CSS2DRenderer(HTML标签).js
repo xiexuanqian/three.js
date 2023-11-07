fa4a84bf31509f8f47b48832f0b115fccda39895
xiexuanqian <1280809174@qq.com>
2023-11-07 16:28:48 +0800

    标签

--- 15_场景标注标签信息/1_CSS2DRenderer(HTML标签).js
+++ 15_场景标注标签信息/1_CSS2DRenderer(HTML标签).js
@@ -1,14 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
-// 引入CSS2渲染器CSS2DRenderer和CSS2模型对象CSS2DObject
-import { CSS2DRenderer,CSS2DObject } from 'three/addons/renderers/CSS2DRenderer.js';
-
-
-const div = document.getElementById('tag');
-// HTML元素转化为threejs的CSS2模型对象
-const tag = new CSS2DObject(div);
-tag.position.set(50,0,50);
+import { CSS2DRenderer, CSS2DObject } from 'three/addons/renderers/CSS2DRenderer.js';
 
 const width = window.innerWidth
 const height = window.innerHeight
@@ -41,33 +34,57 @@ scene.add(mesh2)
 scene.add(camera)
 scene.add(axes)
 scene.add(envLight)
-scene.add(tag);
 
 const renderer = new THREE.WebGLRenderer()
-// 创建一个CSS2渲染器CSS2DRenderer
-const css2Renderer = new CSS2DRenderer();
 // width, height：canvas画布宽高度
-css2Renderer.setSize(width, height);
-
-// 用法和webgl渲染器渲染方法类似
-css2Renderer.render(scene, camera);
-// renderer.render(scene, camera);
+renderer.setSize(width, height);
+renderer.render(scene, camera);
 
 
-renderer.setSize(width, height)
-renderer.shadowMap.enabled = true;
 
-renderer.render(scene, camera)
+document.body.appendChild(renderer.domElement)
 
-const orbit = new OrbitControls(camera, renderer.domElement)
+
+const earthDiv = document.createElement('div');
+earthDiv.className = 'label';
+earthDiv.textContent = '长方体';
+earthDiv.style.backgroundColor = 'transparent';
+const EARTH_RADIUS = 1;
+const earthLabel = new CSS2DObject(earthDiv);
+earthLabel.position.set(1.5 * EARTH_RADIUS, 0, 0);
+earthLabel.center.set(0, 1);
+mesh.add(earthLabel);
+earthLabel.layers.set(0);
+
+const earthMassDiv = document.createElement('div');
+earthMassDiv.className = 'label';
+earthMassDiv.textContent = '正方体';
+earthMassDiv.style.backgroundColor = 'transparent';
+
+const earthMassLabel = new CSS2DObject(earthMassDiv);
+// earthMassLabel.position.set(1.5 * 2, 0, 0);
+mesh2.add(earthMassLabel);
+earthMassLabel.layers.set(0);
+
+const labelRenderer = new CSS2DRenderer();
+// 必须和renderer的画布大小一样大
+labelRenderer.setSize(window.innerWidth, window.innerHeight);
+
+// 必须要写
+labelRenderer.domElement.style.position = 'absolute';
+labelRenderer.domElement.style.top = '0px';
+labelRenderer.render(scene, camera);
+document.body.appendChild(labelRenderer.domElement);
+
+
+const orbit = new OrbitControls(camera, labelRenderer.domElement)
 orbit.addEventListener('change', () => {
-    renderer.render(scene, camera)
+    renderer.render(scene, camera);
+    labelRenderer.render(scene, camera);
 })
-
-// document.body.appendChild(renderer.domElement)
-document.body.appendChild(css2Renderer.domElement);
-
-
-
-
-
+function render(){
+    renderer.render(scene, camera);
+    labelRenderer.render(scene, camera);
+    requestAnimationFrame(render)
+}
+render()
